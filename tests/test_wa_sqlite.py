dee1a55c24aa594bdceb27d1584b62b39487ef19
Chaps SD <drumchaps@gmail.com>
2019-10-25 21:14:47 -0500

    Use .travis.yml script oneliners and update tests select fields for test cases

--- tests/test_wa_sqlite.py
+++ tests/test_wa_sqlite.py
@@ -25,12 +25,12 @@ def test_get_update_chatlist_query():
     pass
 
 def test_get_messages_query():
-    assert WA_SQLITE.get_messages() == f"SELECT * FROM {WA_SQLITE.MESSAGES_TABLE_NAME};"
+    assert WA_SQLITE.get_messages() == f"SELECT _id, key_remote_jid, key_from_me, key_id, status, data, timestamp FROM {WA_SQLITE.MESSAGES_TABLE_NAME};"
 
 
 def test_get_messages_query_filter_jid():
     assert WA_SQLITE.get_messages(key_remote_jid=TEST_JID) == \
-    f"SELECT * FROM {WA_SQLITE.MESSAGES_TABLE_NAME} WHERE{WA_SQLITE.FILTER_FROM_JID.format(key_remote_jid=TEST_JID)};" 
+    f"SELECT _id, key_remote_jid, key_from_me, key_id, status, data, timestamp FROM {WA_SQLITE.MESSAGES_TABLE_NAME} WHERE{WA_SQLITE.FILTER_FROM_JID.format(key_remote_jid=TEST_JID)};" 
 
 
 def test_get_messages_query_filter_from_date():
@@ -38,21 +38,21 @@ def test_get_messages_query_filter_from_date():
     """
     t = time.time()
     assert WA_SQLITE.get_messages(from_date=t) == \
-    f"SELECT * FROM {WA_SQLITE.MESSAGES_TABLE_NAME} WHERE{WA_SQLITE.FILTER_FROM_DATE.format(filter_from_date_epoch=t)};" 
+    f"SELECT _id, key_remote_jid, key_from_me, key_id, status, data, timestamp FROM {WA_SQLITE.MESSAGES_TABLE_NAME} WHERE{WA_SQLITE.FILTER_FROM_DATE.format(filter_from_date_epoch=t)};" 
 
 def test_get_messages_query_filter_to_date():
     """
     """
     t = time.time()
     assert WA_SQLITE.get_messages(to_date=t) == \
-    f"SELECT * FROM {WA_SQLITE.MESSAGES_TABLE_NAME} WHERE{WA_SQLITE.FILTER_TO_DATE.format(filter_to_date_epoch=t)};" 
+    f"SELECT _id, key_remote_jid, key_from_me, key_id, status, data, timestamp FROM {WA_SQLITE.MESSAGES_TABLE_NAME} WHERE{WA_SQLITE.FILTER_TO_DATE.format(filter_to_date_epoch=t)};" 
 
 def test_get_messages_query_filter_jid_and_from_date():
     """
     """
     t = time.time()
     assert WA_SQLITE.get_messages(key_remote_jid=TEST_JID, from_date=t) == \
-    f"SELECT * FROM {WA_SQLITE.MESSAGES_TABLE_NAME} WHERE{WA_SQLITE.FILTER_FROM_JID.format(key_remote_jid=TEST_JID)}AND{WA_SQLITE.FILTER_FROM_DATE.format(filter_from_date_epoch=t)};" 
+    f"SELECT _id, key_remote_jid, key_from_me, key_id, status, data, timestamp FROM {WA_SQLITE.MESSAGES_TABLE_NAME} WHERE{WA_SQLITE.FILTER_FROM_JID.format(key_remote_jid=TEST_JID)}AND{WA_SQLITE.FILTER_FROM_DATE.format(filter_from_date_epoch=t)};" 
 
 
 ###
